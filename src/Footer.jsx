03f4308f847bd4e2ede1b40eb3b033fc2a23578e
abdellah khouden <khoudenak@gmail.com>
2024-12-13 00:49:48 +0100

    add Footer and Food components; update App and Header for layout

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+
+function Footer(){
+
+    return (
+        <footer>
+            <p>
+                &copy; Your website {new Date().getFullYear()}
+            </p>
+        </footer>
+    );
+}
+
+
+export default Footer;
