d258a569055ef1be05fded53f46ceb666ab988e4
abdellah khouden <khoudenak@gmail.com>
2024-12-13 02:07:07 +0100

    PROPS in React explained

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Button from "./Button.jsx"
+import Student from "./Student.jsx"
 
 function App() {
   return (
     <>
-    <Button></Button>
+      <Student name="Abdellah" age={22} isStudent={true}/>
+      <Student name="Othman" age={20} isStudent={false}/>
+      <Student name="Maryame" age={24} isStudent={false}/>
+      <Student name="Zakariya" age={10} isStudent={true}/>
+      <Student name="aymane"/>
     </>
   );
 }
